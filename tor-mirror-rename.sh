a0f190fc628b9c819ca7f949049558f08f9d8a89
Matt Pagan <matthew.a.pagan@gmail.com>
2013-05-30 09:27:46 -0500

    Added a little documentation and some variables

--- tor-mirror-rename.sh
+++ tor-mirror-rename.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
+# tor-mirror-rename.sh
+
+## This script is for anyone who wants to run an unlisted torproject.org
+## mirror. This will replace the names of some website subdirectories, 
+## which may make the mirror harder to block. 
+
+## This script should be added to a cron job like so:
+## 0 */6 * * * tor-mirror-rename.sh
+
 ARGS="-aq --delete"
 SITE="rsync://rsync.torproject.org/website-mirror"
 LOCATION="/var/www/"
+NEWDIST="d1st"
+NEWTORBROWSER="t0rbr0wser"
 
 rsync $ARGS $SITE $LOCATION
-mv $LOCATION/dist/t0rbr0wser
-mv $LOCATION/dist $LOCATION/d1st
+mv $LOCATION/dist/$NEWTORBROWSER
+mv $LOCATION/dist $LOCATION/$NEWDIST
 ## all files of the form foo/dist/torbrowser/bar are 
-## now known as foo/d1st/t0rbr0wser/bar
+## now known as foo/$NEWDIST/$NEWTORBROWSER/bar
 
 replace_strings () {
-  sed 's/dist\/torbrowser/d1st\/t0rbr0wser/g' *.html.en > $dir.new
-    ## Makes a new file with good file paths
+  sed "s/dist\/torbrowser/$NEWDIST\/$NEWTORBROWSER/g" *.html.en > $dir.new
+    ## Makes a new file with the newly named file paths
   rm -f $dir ## This is the html file without file paths replaced
   mv $dir.new $dir ## Now it's back
 }
@@ -31,4 +42,4 @@ done
  
 walk()
 
-exit 0;
+exit 0
