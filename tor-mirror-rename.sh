f9dacd4bfc8916d5bdd605e4c0486cb2e1a59073
Matt Pagan <matthew.a.pagan@gmail.com>
2013-06-04 05:48:49 -0500

    Made the script copy the folder d1st from dist instead of replacing it, which means I don't have to download all the dist folder all over again each time I update.

--- tor-mirror-rename.sh
+++ tor-mirror-rename.sh
@@ -8,38 +8,41 @@
 ## This script should be added to a cron job like so:
 ## 0 */6 * * * tor-mirror-rename.sh
 
-ARGS="-aq --delete"
+ARGS="-aq"
 SITE="rsync://rsync.torproject.org/website-mirror"
-LOCATION="/var/www/"
+LOCATION="/var/www"
 NEWDIST="d1st"
 NEWTORBROWSER="t0rbr0wser"
 
 rsync $ARGS $SITE $LOCATION
-mv $LOCATION/dist/torbrowser $LOCATION/dist/$NEWTORBROWSER
-mv $LOCATION/dist $LOCATION/$NEWDIST
+
+cd $LOCATION
+mkdir -p $NEWDIST
+rsync -a $LOCATION/dist $LOCATION/$NEWDIST
+mv $LOCATION/$NEWDIST/torbrowser $LOCATION/$NEWDIST/$NEWTORBROWSER
 ## all files of the form foo/dist/torbrowser/bar are 
-## now known as foo/$NEWDIST/$NEWTORBROWSER/bar
+## now also known as foo/$NEWDIST/$NEWTORBROWSER/bar
 
-replace_strings () {
-  sed "s/dist\/torbrowser/$NEWDIST\/$NEWTORBROWSER/g" $dir > $dir.new
-    ## Makes a new file with the newly named file paths
-  rm $dir ## This is the html file without file paths replaced
-  mv $dir.new $dir ## Now it's back
-}
+#replace_strings () {
+#  sed "s/dist\/torbrowser/$NEWDIST\/$NEWTORBROWSER/g" $dir > $dir.new
+#    ## Makes a new file with the newly named file paths
+#  rm $dir ## This is the html file without file paths replaced
+#  mv $dir.new $dir ## Now it's back
+#}
+#
+#walk () {
+#for dir in `echo *`
+#do
+#   if [ -d "$dir" ] ; then # If it is a directory
+#     cd $dir               # enter the directory
+#     walk()                # and execute this script recursively
+#     cd ..                 # then leave the directory
+#   else                    # Not a directory
+#     replace_strings()
+#   fi
+#done
+#}
+# 
+#walk();
+exit 0;
 
-walk () {
-for dir in `echo *`
-do
-   if [ -d "$dir" ] ; then # If it is a directory
-     cd $dir               # enter the directory
-     walk()                # and execute this script recursively
-     cd ..                 # then leave the directory
-   else                    # Not a directory
-     replace_strings()
-   fi
-done
-}
- 
-walk()
-
-exit 0
